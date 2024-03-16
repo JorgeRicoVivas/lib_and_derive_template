c1d924140acc86c8f0ad3f451661af40e6a26080
Jorge Rico Vivas <109862275+JorgeRicoVivas@users.noreply.github.com>
2024-03-16 20:38:19 +0100

    Add files via upload

---
+++ derive/src/lib.rs
@@ -0,0 +1,19 @@
+use proc_macro::TokenStream;
+use syn::{DeriveInput, parse_macro_input};
+
+
+#[proc_macro_derive(MyDerive, attributes(MyDeriveAttributeA, MyDeriveAttributeB))]
+pub fn my_proc_macro_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+	TokenStrean::new()
+}
+
+#[proc_macro_attribute]
+pub fn my_proc_macro_attribute(attr: TokenStream, mut item: TokenStream) -> TokenStream {
+    item
+}
+
+#[proc_macro]
+pub fn my_proc_macro(item: TokenStream) -> TokenStream {
+    TokenStrean::new()
+}
